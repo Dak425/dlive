45fe88dea1af37a0bb4ad046db9e24e9fafbce14
Dak425 <highlordazurai425@gmail.com>
2019-04-28 22:28:50 -0400

    Added more tests for feed

--- pkg/api/feed_test.go
+++ pkg/api/feed_test.go
@@ -31,3 +31,44 @@ func TestFeed_Active(t *testing.T) {
 	}
 }
 
+func TestFeed_Publish(t *testing.T) {
+	chan1 := make(chan []byte)
+	chan2 := make(chan []byte)
+
+	f := Feed{
+		subscriptions: map[string]chan<- []byte{
+			"one": chan1,
+			"two": chan2,
+		},
+	}
+
+	p := []byte{'h','e','l','l','o'}
+
+	res, err := f.Publish(p)
+
+	if err != nil {
+		t.Error("got error: ", err)
+	}
+
+	expected := len(p) * 2
+
+	if res != expected {
+		t.Errorf("expected return length of %d, got %d", expected, res)
+	}
+}
+
+func TestFeed_Subscribe(t *testing.T) {
+	f := Feed{
+		subscriptions: make(map[string]chan<- []byte),
+	}
+
+	s, err := f.Subscribe()
+
+	if err != nil {
+		t.Error("error when subscribing to feed: ", err)
+	}
+
+	if s.feed != &f {
+		t.Error("subscription has incorrect feed reference")
+	}
+}
